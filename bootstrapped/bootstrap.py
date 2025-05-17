d15807ac9b8848f2be8cb0863530f0802e237d7b
award96 <anders.s.ward@gmail.com>
2025-05-17 11:12:50 -0700

    wrote basic bootstrap code

--- bootstrapped/bootstrap.py
+++ bootstrapped/bootstrap.py
@@ -1,3 +1,104 @@
+from turtle import pd
+from typing import (
+    Callable,
+    Iterable,
+    Optional,
+    Union,
+    Dict
+)
 import polars as pl
 from . import generate
 from . import bootstrap_result
+
+def bootstrap_simulation(
+    observed: Iterable[Union[Iterable[Union[int, float]], int, float]],
+    statistics: Iterable[Callable],
+    confidence_level: float=0.95,
+    n_bootstraps: int=100,
+    seed: int=10
+) -> Dict[str, bootstrap_result.BootstrapResults]:
+    """Estimate the mean, upper, and lower bounds of an array of arrays
+    for a list of statistics. The result will be an estimate for each
+    array of observed samples, for each statistic.
+
+    Args:
+        observed: An array of arrays of observed samples, or a singular array.
+        statistics: A list of statistics to estimate.
+        confidence_level: The confidence level to use for the bootstrap.
+        n_bootstraps: The number of bootstraps to use.
+        seed: The seed to use for the bootstrap.
+    Returns:
+        A dictionary of bootstrap results, with the statistic name as the key.
+    """
+    observed: pl.LazyFrame = arrays_to_pl_lazyframe(observed)
+    bootstrap_sample_indices: pl.LazyFrame = generate.create_index_matrix(
+        n_rows=len(observed),
+        n_bootstraps=n_bootstraps,
+        seed=seed
+    )
+    bootstrap_results: Dict[str, bootstrap_result.BootstrapResults] = {
+        stat.__name__: [
+            pl.Series([None]*n_bootstraps).alias(stat.__name__) for _ in range(observed.shape[0])
+        ] for stat in statistics
+    }
+    for i in range(n_bootstraps):
+        for input_idx, obs in enumerate(observed):
+            bs_sample = obs.select(pl.all().gather(
+                 bootstrap_sample_indices.select(str(i))
+            ))
+            for j, stat in enumerate(statistics):
+                result = stat(bs_sample.collect())
+                result = bootstrap_result.BootstrapResults(
+                    lower_bound=result.lower_bound,
+                    value=result.value,
+                    upper_bound=result.upper_bound
+                )
+                bootstrap_results[stat.__name__][input_idx][i] = result
+    return bootstrap_results
+        
+
+def arrays_to_pl_lazyframe(
+        observed: Iterable[Union[Iterable[Union[int, float]], int, float]]
+    ) -> pl.LazyFrame:
+    """For each array in the observed array, create a column in the output lazyframe.
+    
+    Args:
+        observed: An array of arrays of observed samples, or a singular array.
+    Returns:
+        A lazyframe with the observed samples as columns.
+    """
+    if isinstance(observed, pl.DataFrame):
+        return observed.lazy()
+    elif isinstance(observed, pl.LazyFrame):
+        return observed
+    return pl.LazyFrame(
+        sub_array_pl.alias(str(i)) for i, sub_array_pl in enumerate(arrays_to_iter_of_series(observed))
+    )
+
+def arrays_to_iter_of_series(
+        observed: Iterable[Union[Iterable[Union[int, float]], int, float]]
+    ) -> Iterable[pl.Series]:
+    """Convert an array of arrays to an iterator of series."""
+    # if observed is not a nested array
+    # and is just a simple array of numbers
+    # TODO does this work for lists, numpy ndarrays, pandas dataframes, pandas series,
+    # torch arrays, etc?
+    if not hasattr(next(observed), "__iter__"):
+        yield pl.Series(observed)
+    else:
+        # TODO check that everything is in the correct format
+        # All sub arrays are of the same length
+        # All sub arrays are of the same dtype
+        # All sub arrays are numeric
+        for sub_array in observed:
+            # TODO how to type check without importing pandas?
+            if isinstance(sub_array, pd.Series):
+                yield pl.from_pandas(sub_array)
+            elif isinstance(sub_array, np.ndarray):
+                yield pl.from_numpy(sub_array)
+            elif isinstance(sub_array, arrow.Array):
+                yield pl.from_arrow(sub_array)
+            elif isinstance(sub_array, pl.Series):
+                yield sub_array
+            else:
+                yield pl.Series(sub_array)
